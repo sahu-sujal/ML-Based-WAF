5ed830500390a6373004d7961e2155ac7849ddf1
Sujal Sahu <sujal020404@gmail.com>
2025-03-03 21:06:17 +0530

    Update settings.py

--- MLWAF/settings.py
+++ MLWAF/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8_q%%461x8)lo!%#td0j(0od-w#^hzzi6d8c9518g#jlk2zb6_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
